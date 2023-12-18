a08dd41e111fa0a8090a9b79dbbb3eeb4ee383fc
stranavadavid <stranavadavid@protonmail.com>
2023-12-18 16:38:19 +0100

    problem solver

--- pages/login.vue
+++ pages/login.vue
@@ -14,6 +14,25 @@ async function login(){
     },});
   console.error(error);
 }
+
+async function logout(){
+  await supabase.auth.signOut();
+  localStorage.clear();
+  sessionStorage.clear();
+
+  try {
+    const cookies = document.cookie;
+    for (let i = 0; i < cookies.split(";").length; ++i)
+    {
+      const myCookie = cookies[i];
+      const pos = myCookie.indexOf("=");
+      const name = pos > -1 ? myCookie.substr(0, pos) : myCookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  } catch {
+
+  }
+}
 </script>
 
 <template>
@@ -21,6 +40,13 @@ async function login(){
     <h2 class="text-3xl font-semibold mb-5">Biologie 24</h2>
     <p class="text-sm dark:text-slate-300 text-slate-700">Aneb jak se naucit poznavacku, s nejmensim usilim</p>
     <UButton class="mt-6 font-semibold" @click="login()">Prihlasit se</UButton>
+    <UButton
+      class="mt-10"
+      size="2xs"
+      @click="logout()"
+    >
+      Vyresit problemy
+    </UButton>
     <div v-if="user" class="mt-10">
       <pre class="dark:text-slate-200 text-slate-800">{{user}}</pre>
     </div>
