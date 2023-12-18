762b982cca9bd668d67736dcff3537bf6f17e360
stranavadavid <stranavadavid@protonmail.com>
2023-12-18 16:27:11 +0100

    f

--- pages/login.vue
+++ pages/login.vue
@@ -21,6 +21,9 @@ async function login(){
     <h2 class="text-3xl font-semibold mb-5">Biologie 24</h2>
     <p class="text-sm dark:text-slate-300 text-slate-700">Aneb jak se naucit poznavacku, s nejmensim usilim</p>
     <UButton class="mt-6 font-semibold" @click="login()">Prihlasit se</UButton>
+    <div v-if="user" class="mt-10">
+      <pre class="dark:text-slate-200 text-slate-800">{{user}}</pre>
+    </div>
   </div>
 </template>
 
