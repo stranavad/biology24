4d3cecd884642014b113705867f3c596a75dec60
gandalfthegray <stranavadavid@protonmail.com>
2024-01-05 13:18:26 +0100

    list

--- server/api/stats/leaderboard.get.ts
+++ server/api/stats/leaderboard.get.ts
@@ -6,12 +6,12 @@ import type {LeaderboardItem} from "~/types/game";
 export default defineEventHandler(async(): Promise<LeaderboardItem[]> => {
   const data = await db.select({
     userId: history.userId,
-    total: sql<number>`cast(count(${history.userId}) as int)`,
     correct: sql<number>`cast(SUM(CASE WHEN ${history.correct} THEN 1 ELSE 0 END) as int)`,
     wrong: sql<number>`cast(SUM(CASE WHEN ${history.correct} THEN 0 ELSE 1 END) as int)`,
     nickname: usersPublic.nickname,
   }).from(history).groupBy(history.userId, usersPublic.nickname, usersPublic.points).innerJoin(usersPublic, eq(history.userId, usersPublic.userId));
   return data.map((item) => {
-    return {...item, points: (item.correct * 10) - (item.wrong * 5) + 100, percentage: Math.round((item.correct / item.total) * 100)}
+    const total = item.correct + item.wrong;
+    return {...item, total, points: (item.correct * 10) - (item.wrong * 5) + 100, percentage: Math.round((item.correct / total) * 100)}
   }).sort((a, b) => b.points - a.points)
 })
