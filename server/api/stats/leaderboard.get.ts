1cf122478781faf36c135c8e98a87a9709c0d087
gandalfthegray <stranavadavid@protonmail.com>
2024-01-05 13:15:58 +0100

    list

--- server/api/stats/leaderboard.get.ts
+++ server/api/stats/leaderboard.get.ts
@@ -10,8 +10,8 @@ export default defineEventHandler(async(): Promise<LeaderboardItem[]> => {
     correct: sql<number>`cast(SUM(CASE WHEN ${history.correct} THEN 1 ELSE 0 END) as int)`,
     wrong: sql<number>`cast(SUM(CASE WHEN ${history.correct} THEN 0 ELSE 1 END) as int)`,
     nickname: usersPublic.nickname,
-  }).from(history).groupBy(history.userId, usersPublic.nickname, usersPublic.points).innerJoin(usersPublic, eq(history.userId, usersPublic.userId)).orderBy(desc(usersPublic.points));
+  }).from(history).groupBy(history.userId, usersPublic.nickname, usersPublic.points).innerJoin(usersPublic, eq(history.userId, usersPublic.userId));
   return data.map((item) => {
-    return {...item, points: (item.correct * 10) - (item.wrong * 5), percentage: Math.round((item.correct / item.total) * 100)}
-  })
+    return {...item, points: (item.correct * 10) - (item.wrong * 5) + 100, percentage: Math.round((item.correct / item.total) * 100)}
+  }).sort((a, b) => b.points - a.points)
 })
